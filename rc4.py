ee31dcc1e8b2fa0745d9de439ac51c76ff49885f
Khairunnisa Rifdah <18218008@std.stei.itb.ac.id>
2021-02-25 20:52:24 +0700

    update encrypt and decrypt

--- rc4.py
+++ rc4.py
@@ -1,3 +1,5 @@
+import binascii
+import codecs
 
 def convert_text(s):
     return ([ord(c) for c in s])
@@ -37,15 +39,48 @@ def prga(x):
         j = (j + x[i]) % 256
         x[i] = x[j]
         x[j] = x[i]
-        result = (x[i] + x[j]) % 256
-        streamkey = x(result)
-        yield streamkey
+        result = x[(x[i] + x[j]) % 256]
+        yield result
+    
+def getkey(key):
+    a = ksa(key)
+    return prga(a)
+
+def encrypt(key, text):
+    key = convert_text(key)
+    t = convert_text(text)
+    k = getkey(key)
+
+    result = []
+    for i in t:
+        r = ("%02X" % (i ^ next(k)))
+        result.append(r)
+    return ''.join(result)
+
+def decrypt(key, text):
+    t = binascii.unhexlify(text)
+    key = convert_text(key)
+    k = getkey(key)
+
+    result = []
+    for i in t:
+        r = ("%02X" % (i ^ next(k)))
+        result.append(r)
+    hasil = ''.join(result)
+    return(codecs.decode(hasil, 'hex_codec').decode('utf-8'))
 
 
-
+    
 b = convert_text('0123!klm')
 c = ksa(b)
 d = ksa1(b)
 e = prga(d)
-print(d)
-print(e)
+
+text = 'hello panda hello panda'
+key = '01qwerty!'
+cipher = 'A8D2373F7BB7C53414375F188CA354E0EFED75E1354035'
+a = encrypt(key, text)
+f = decrypt(key,cipher)
+
+print(a)
+print(f)
