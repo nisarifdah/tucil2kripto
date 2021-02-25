a060679bc5e60d84a38a2cc3a9681ef1cca4961b
adihendro <adi21hendro@gmail.com>
2021-02-25 22:18:52 +0700

    add extend key function

--- lsfr.py
+++ lsfr.py
@@ -1,4 +1,5 @@
-def lsfr(key):
+# Return lsfr from key in binary
+def getLsfr(key):
     binary = ''.join(format(ord(i), '08b') for i in key)
     l = len(binary)
     output = []
@@ -6,5 +7,10 @@ def lsfr(key):
         output.append(binary[l-1])
         binary = str(int(binary[0]) ^ int(binary[l-1])) + binary[0:l-1]
     return output
-    
-print(lsfr('a'))
+
+# Key is shorter than 32 characters (256 bits) 
+def extendKey(key):
+    key = list(key)
+    for i in range (32 - len(key)): # 32 characters for KSA
+        key.append(key[i % len(key)])
+    return ''.join(key)
